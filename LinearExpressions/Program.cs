3d51522097ccf55a4bc48b2b511e4a641ccc0694
Nazarii Tyshkevych <lazzy.aim@gmail.com>
2021-10-21 13:31:03 +0300

    Commit
    ISM courses

---
+++ LinearExpressions/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinearExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, x, y, z, r;
+
+            Console.WriteLine("Enter number - a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number - b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number - c");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number - d");
+            d = Convert.ToDouble(Console.ReadLine());
+
+            x = (a + 2 * b - c + d) / (c * d) + (a + b) / (c - d) - (Math.Pow(a, 2)) / (Math.Pow(b, 2));
+            y = (5 * (a + b) * (c - d)) / ((1 / 2) * c) + Math.Pow(d, 2) * ((Math.Pow(a, 2) - Math.Pow(b, 2)) / (b - a));
+            z = ((Math.Pow((Math.Pow(x, 2) - 2 * x), 3)) - 4 * (Math.Pow(x, 4) + 1)) * (1 - b) / (5 * a + 3 * b);
+            r = ((1 / 2) * a + (3 / 4) * b - (7 / 5)) / (3 * c + 1) + (1) / (a - c);
+
+            Console.WriteLine("Result x:");
+            Console.WriteLine(x);
+            Console.WriteLine("Result y:");
+            Console.WriteLine(y);
+            Console.WriteLine("Result z:");
+            Console.WriteLine(z);
+            Console.WriteLine("Result r:");
+            Console.WriteLine(r);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace QuadraticEquation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d, x1, x2;
+
+            Console.WriteLine("Enter number - a");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number - b");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter number - c");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            d = Math.Pow(b, 2) - (4 * a * c);
+
+            if (d > 0)
+            {
+                x1 = (-b + (4 * a * c)) / (2 * a);
+                x2 = (-b - (4 * a * c)) / (2 * a);
+
+                Console.WriteLine("First root:");
+                Console.WriteLine(x1);
+                Console.WriteLine("Second root:");
+                Console.WriteLine(x2);
+            }
+            else if (d == 0)
+            {
+                x1 = (-b + (4 * a * c)) / (2 * a);
+
+                Console.WriteLine("Root:");
+                Console.WriteLine(x1);
+            }
+            else
+            {
+                Console.WriteLine("Equation doesn't have roots");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
